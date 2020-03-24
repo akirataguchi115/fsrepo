979b0751da81d655c123836eef3e34656611db01
Akira Taguchi <akirahattivatti@gmail.com>
2020-03-24 11:02:20 +0200

    1.7 done

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -36,7 +36,9 @@ const App = () => {
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
-
+      <div>all {good + neutral + bad}</div>
+      <div>average {(good * 1 + bad * -1) / (good + neutral + bad)}</div>
+      <div>positive {good / (good + neutral + bad)} %</div>
     </div>
 
   )
