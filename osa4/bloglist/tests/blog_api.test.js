ae673c3d5af6a04232961b1fa4fa789db6540491
Akira Taguchi <akirahattivatti@gmail.com>
2020-07-28 17:39:58 +0300

    4.8 done

---
+++ osa4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: 'Steve Jobs',
+    author: 'Walter Isaacson',
+    url: 'https://www.amazon.com/Steve-Jobs-Walter-Isaacson/dp/1451648537',
+    likes: 6
+  },
+  {
+    title: 'Nalle Puh',
+    author: 'A. A. Milne',
+    url: 'https://www.adlibris.com/fi/kirja/nalle-puh-maailman-paras-karhu---kirja-jossa-koemme-nalle-puhin-seurassa-seikkailujen-vuoden-puolen-hehtaarin-puistossa-9789510423004',
+    likes: 6
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(initialBlogs.length)
+})
+
+test('a specific blog is within the returned blogs', async () => {
+  const response = await api.get('/api/blogs')
+
+  const contents = response.body.map(r => r.title)
+
+  expect(contents).toContain(
+    'Nalle Puh'
+  )
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
