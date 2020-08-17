8baadf0185bf21af5cdc0cb5b8206c2e74ec480c
Akira Taguchi <akirahattivatti@gmail.com>
2020-08-17 18:33:55 +0300

    4.15 done

---
+++ osa4/bloglist/tests/test_helper.js
@@ -0,0 +1,16 @@
+const Blog = require('../models/blog')
+const User = require('../models/user')
+
+const blogsInDb = async () => {
+  const notes = await Blog.find({})
+  return notes.map(note => note.toJSON())
+}
+
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(user => user.toJSON())
+}
+
+module.exports = {
+  blogsInDb, usersInDb
+}
