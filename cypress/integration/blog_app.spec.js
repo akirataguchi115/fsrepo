07220cb3638ee9fb921ff404d40cc365fba8aaa5
Akira Taguchi <akirahattivatti@gmail.com>
2021-10-20 16:13:32 +0300

    Complete 5.18

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -1,10 +1,34 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
   it('Login form is shown', function() {
     cy.contains('Log in to application')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+
+      cy.contains('Matti Luukkainen logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('vaara-tunnus')
+      cy.get('#password').type('viela-vaarempi-salasana')
+      cy.get('#login-button').click()
+
+      cy.contains('wrong username or password')
+    })
   })
 })
