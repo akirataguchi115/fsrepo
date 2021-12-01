5a98f6b0172c343ff60231b1d69ebc4eac10b442
Akira Taguchi <akirahattivatti@gmail.com>
2021-12-01 13:35:37 +0200

    Complete 7.4

--- osa7/routed-anecdotes/src/App.js
+++ osa7/routed-anecdotes/src/App.js
@@ -9,6 +9,8 @@ import {
   useRouteMatch,
 } from 'react-router-dom'
 
+import { useField } from './hooks'
+
 const Menu = () => {
   const padding = {
     paddingRight: 5
@@ -56,17 +58,17 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
-
+  // const [content, setContent] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
   }
@@ -77,15 +79,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input name='content' value={content.value} onChange={(e) => content.onChange(e)} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input name='author' value={author.value} onChange={(e) => author.onChange(e)} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+          <input name='info' value={info.value} onChange={(e) => info.onChange(e)} />
         </div>
         <button>create</button>
       </form>
@@ -173,8 +175,8 @@ const App = () => {
           </Route>
           <Route path="/create">
             {notification
-            ? <Redirect to="/" />
-            : <CreateNew addNew={addNew} />}
+              ? <Redirect to="/" />
+              : <CreateNew addNew={addNew} />}
           </Route>
           <Route path="/about">
             <About />
