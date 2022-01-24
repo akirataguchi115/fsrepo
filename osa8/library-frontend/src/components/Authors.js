f41fef5c39504a0e5d76765cde479f7e871d2cf3
Akira Taguchi <akirahattivatti@gmail.com>
2022-01-24 15:11:40 +0200

    Complete 8.11

--- osa8/library-frontend/src/components/Authors.js
+++ osa8/library-frontend/src/components/Authors.js
@@ -1,13 +1,38 @@
-import React from 'react'
-import { useQuery } from '@apollo/client'
-import { ALL_AUTHORS } from '../queries'
+import React, { useState, useEffect } from 'react'
+import { useQuery, useMutation } from '@apollo/client'
+import { ALL_AUTHORS, EDIT_AUTHOR } from '../queries'
 
 const Authors = (props) => {
   const authors = useQuery(ALL_AUTHORS)
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
+
+  const [ changeBorn, result] = useMutation(EDIT_AUTHOR, {
+    onError: (error) => {
+      console.log(JSON.stringify(error.networkError.result.errors[0].message))
+    },
+    refetchQueries: [ { query: ALL_AUTHORS } ]
+  })
+
+  useEffect(() => {
+    if ( result.data && !result.data.editAuthor) {
+      console.log('name not found')
+    }
+  }, [result.data])
+
   if (authors.loading) return <div>loading...</div>
 
   if (!props.show) {
     return null
+  }
+
+  const submit = async (event) => {
+    event.preventDefault()
+
+    changeBorn({ variables: { name, setBornTo: born }})
+
+    setName('')
+    setBorn('')
   }
 
   return (
@@ -33,7 +58,22 @@ const Authors = (props) => {
           )}
         </tbody>
       </table>
-
+      <h3>Set birthyear</h3>
+      <form onSubmit={submit}>
+        <div>
+          name
+          <input
+          value={name}
+          onChange={({ target }) => setName(target.value)}/>
+        </div>
+        <div>
+          born
+          <input
+          value={born}
+          onChange={({ target }) => setBorn(parseInt(target.value))}/>
+        </div>
+        <button type='submit'>update author</button>
+      </form>
     </div>
   )
 }
